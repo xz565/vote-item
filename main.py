ba1cd2f69c21c94ff06c30fff5bc231770267cea
xiaodong <zhang6078349@163.com>
2012-12-12 17:39:38 -0500

    add function to add category and item

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ import webapp2
 class Account(db.Model):
     user_id = db.StringProperty()
 
+
 class Category(db.Model):
     cat_name = db.StringProperty()
-    user_name = db.StringProperty()
-
+    items = db.StringListProperty()
 
 class Vote(db.Model):
     owner = db.StringProperty()
@@ -51,15 +51,96 @@ class MainHandler(webapp2.RequestHandler):
             account = Account(key_name=uid, user_id=uid)
             account.put()
 
+
 class OptionHandler(webapp2.RequestHandler):
     def get(self):
         option = self.request.get("option")
+        new_cat = self.request.get("new_cat")
+        new_item = self.request.get("new_item")
+
         if option == 'manage':
-            pass
+            self.manage_page()
+
         elif option == 'vote':
-            pass
+            self.account_page()
+
+        if new_cat:
+            self.add_category(new_cat)
+            self.manage_page()
+
+        if new_item:
+            self.add_item(new_item)
+            self.manage_page()
+
+
+    def add_item(self, new_item):
+        currt_user = users.get_current_user()
+        cat_name = self.request.get("category")
+        cat_key = db.Key.from_path('Account', currt_user.nickname(), 'Category', cat_name)
+
+        categories = Category.all()
+        categories.ancestor(cat_key)
+
+        for category in categories:
+            items = category.items
+            if new_item not in items:
+                items.append(new_item)
+                category.put()
+            break
+
+
+    def add_category(self, new_cat):
+        currt_user = users.get_current_user()
+        accounts = Account.all()
+        accounts.filter("user_id =", currt_user.nickname())
+
+        for account in accounts:
+            cat = Category(key_name=new_cat, parent=account, cat_name=new_cat)
+            cat.put()
+            break
+            
+
+    def manage_page(self):
+        currt_user = users.get_current_user()
+
+        account_key = db.Key.from_path('Account', currt_user.nickname())
+        categories = Category.all()
+        categories.ancestor(account_key)
+
+        template_values = {
+            'categories': categories
+        }
+        path = os.path.join(os.path.dirname(__file__), 'manage.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+    def account_page(self):
+        accounts = Account.all()
+        template_values = {
+            'accounts': accounts
+        }
+        path = os.path.join(os.path.dirname(__file__), 'accounts.html')
+        self.response.out.write(template.render(path, template_values))
+
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/option', OptionHandler)
 ], debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
