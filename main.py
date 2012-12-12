4b7ae7cdc29eefa16c68e6e2cadec31c78b787d7
xiaodong <zhang6078349@163.com>
2012-12-12 12:33:22 -0500

    added database models

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
+from google.appengine.ext import db
 from google.appengine.api import users
 import webapp2
+
+
+class UserPrefs(db.Model):
+    userid = db.StringProperty()
+
+
+class Category(db.Model):
+    cat_name = db.StringProperty()
+    user_name = db.StringProperty()
+
+
+class Vote(db.Model):
+    owner = db.StringProperty()
+    category = db.StringProperty()
+    win = db.StringProperty()
+    lose = db.StringProperty()
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %(user.nickname(), users.create_logout_url("/")))
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" \
+                            %(user.nickname(), users.create_logout_url("/")))
         else:
-            greeting = ("<a href=\"%s\">Sign in or register</a>." %users.create_login_url("/"))
+            greeting = ("<a href=\"%s\">Sign in or register</a>." \
+                            %users.create_login_url("/"))
 
         self.response.out.write("<html><body>%s</body></html>" % greeting)
 
