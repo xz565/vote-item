1234cde97d4844598dfd05b0c986655977969310
xiaodong <zhang6078349@163.com>
2012-12-12 14:11:13 -0500

    when a new user signs in, add a new Account info

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from google.appengine.api import users
 import webapp2
 
 
-class UserPrefs(db.Model):
-    userid = db.StringProperty()
-
+class Account(db.Model):
+    user_id = db.StringProperty()
 
 class Category(db.Model):
     cat_name = db.StringProperty()
@@ -28,6 +27,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
+            self.checkUser(user)
+
             template_values = {
                 'nickname': user.nickname(),
                 'logout_url': users.create_logout_url("/")
@@ -38,6 +39,25 @@ class MainHandler(webapp2.RequestHandler):
             greeting = ("<a href=\"%s\">Sign in or register</a>." \
                             %users.create_login_url("/"))
             self.response.out.write("<html><body>%s</body></html>" % greeting)
+
+    def checkUser(self, user):
+        accounts = Account.all()
+        user_ids = []
+        for acc in accounts:
+            user_ids.append(acc.user_id)
+
+        uid = user.nickname()
+        if uid not in user_ids:
+            account = Account(key_name=uid, user_id=uid)
+            account.put()
+
+class OptionHandler(webapp2.RequestHandler):
+    def get(self):
+        option = self.request.get("option")
+        if option == 'manage':
+            pass
+        elif option == 'vote':
+            pass
 
 
 app = webapp2.WSGIApplication([
